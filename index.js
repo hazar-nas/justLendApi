bf236aadb299ce9c7c3d7a80c9a56f808cbefa09
hdraptorr <nascan61.@gmail.com>
2022-10-07 12:53:06 +0300

    fix: local port

--- index.js
+++ index.js
@@ -14,4 +14,7 @@ app.use(cookieParser())
 app.use(express.json())
 app.use(router)
 
-app.listen(5000, () => console.log('Server running at port 5000'))
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`)
+})
