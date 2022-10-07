556fbe2cea10d6c900b738024e4c07086d10c68a
hdraptorr <nascan61.@gmail.com>
2022-10-07 16:42:35 +0300

    fix: cors problem

--- index.js
+++ index.js
@@ -10,18 +10,7 @@ app.get('/', (req, res) => {
   res.json({ message: 'hi' })
 })
 
-var allowlist = ['https://main--lovely-dieffenbachia-81aa0c.netlify.app/']
-var corsOptionsDelegate = function (req, callback) {
-  var corsOptions
-  if (allowlist.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: true }
-  } else {
-    corsOptions = { origin: false }
-  }
-  callback(null, corsOptions)
-}
-
-app.use(cors(corsOptionsDelegate))
+app.use(cors())
 app.use(cookieParser())
 app.use(express.json())
 
