e2cbc64f1761e2294f097e621537556de5250dac
hdraptorr <nascan61.@gmail.com>
2022-10-07 13:11:20 +0300

    last

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cookieParser())
 app.use(express.json())
 app.use(router)
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`)
 })
