b3fe920dff0c3573a9ec8eb8220e864362971d06
hdraptorr <nascan61.@gmail.com>
2022-10-07 15:15:23 +0300

    feat: enabling cors

--- index.js
+++ index.js
@@ -11,7 +11,12 @@ const app = express()
 app.get('/', (req, res) => {
   res.json({ message: 'hi' })
 })
-app.use(cors())
+const corsOptions = {
+  origin: 'https://main--lovely-dieffenbachia-81aa0c.netlify.app/',
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
 app.use(cookieParser())
 app.use(express.json())
 
